fe462b5183cdcd76ccc63b2583e05844d5531e15
chenbingyang <chenbingyang@gracepsy.com>
2018-01-09 16:30:38 +0800

    redux能用了

--- app/containers/User/UserList.js
+++ app/containers/User/UserList.js
@@ -1,7 +1,20 @@
 import React from 'react'
 class UserList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initDone: false
+    }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        initDone: true
+      })
+    }, 1000)
+  }
   render() {
-    return <div>UserList</div>
+    return <div>{this.state.initDone ? <p>done</p> : <p>加载中...</p>}</div>
   }
 }
 
--- app/containers/index.js
+++ app/containers/index.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import RouteMap from '../router'
 import Header from '../components/Header/Header'
+import LocalStore from '../util/localStore'
+import {CITYNAME} from '../config/localStoreKey'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import * as userInfoActionsFromOtherFile from '../actions/userInfo'
 
-const App = () => {
-  return (
-    <div className="app">
-      <Header title="hello world" />
-      <RouteMap />
-      <p>footer</p>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="app">
+        <Header title="hello world" />
+        <RouteMap />
+        <p>footer</p>
+      </div>
+    )
+  }
+  componentDidMount() {
+    let cityName = LocalStore.getItem(CITYNAME)
+    if (cityName == null) {
+      cityName = '北京'
+    }
+    this.props.userInfoActions.update({
+      cityName: cityName
+    })
+  }
 }
-export default App
+function mapStateToProps(state) {
+  return {}
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch)
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
